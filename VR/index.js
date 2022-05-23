933cc834378510a755caeddf07e542893ea1919e
QuintenVanDinther <55178963+QuintenVanDinther@users.noreply.github.com>
2022-05-23 10:16:40 +0200

    Update index.js

--- VR/index.js
+++ VR/index.js
@@ -76,6 +76,12 @@ function onSessionStarted(_session) { // this function defines what happens when
 		0.0, 0.0, 1.0, 0.0,
 		-2.0, 1.0, -5.0, 1.0
 	]);
+	const offsetMatrixCone = new Float32Array([
+		1.0, 0.0, 0.0, 0.0,
+		0.0, 1.0, 0.0, 0.0,
+		0.0, 0.0, 1.0, 0.0,
+		2.0, 1.0, -5.0, 1.0
+	]);
 	
 	const planeMesh = new ezgfx.Mesh();
 	planeMesh.loadFromOBJ("./plane.obj");
@@ -106,6 +112,16 @@ function onSessionStarted(_session) { // this function defines what happens when
 	cilinderMaterial.setModel(offsetMatrixCilinder);
 
 	cilinderMaterial.setColor([0.6, 0.2, 1.0, 1.0]);
+
+	const coneMesh = new ezgfx.Mesh();
+	coneMesh.loadFromOBJ("./cilinder.obj");
+
+	const coneMaterial = new ezgfx.Material();
+	coneMaterial.setProjection(identityMatrix);
+	coneMaterial.setView(identityMatrix);
+	coneMaterial.setModel(offsetMatrixCone);
+
+	coneMaterial.setColor([0.6, 0.2, 1.0, 1.0]);
 
 	xrSession.requestReferenceSpace("local-floor").then((refSpace) => { // we request our referance space - an object that defines where the center of our space lies. Here we request a local-floor referance space - that one defines the center of the world to be where the center of the ground is
 		xrRefSpace = refSpace; // we set our referance space to be the one returned by this function
@@ -143,6 +159,11 @@ function onSessionStarted(_session) { // this function defines what happens when
 				cilinderMaterial.setView(view.transform.inverse.matrix);
 				
 				renderer.draw(cilinderMesh, cilinderMaterial);
+
+				coneMaterial.setProjection(view.projectionMatrix);
+				coneMaterial.setView(view.transform.inverse.matrix);
+				
+				renderer.draw(coneMesh, coneMaterial);
 			}
 		}
 	}
