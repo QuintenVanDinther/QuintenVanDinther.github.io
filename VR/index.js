3f73dc5affecec6dee65be081b0f83f78b6b29b3
QuintenVanDinther <55178963+QuintenVanDinther@users.noreply.github.com>
2022-05-23 09:34:27 +0200

    Update index.js

--- VR/index.js
+++ VR/index.js
@@ -74,7 +74,7 @@ function onSessionStarted(_session) { // this function defines what happens when
 		1.0, 0.0, 0.0, 0.0,
 		0.0, 1.0, 0.0, 0.0,
 		0.0, 0.0, 1.0, 0.0,
-		2.0, 1.0, 5.0, 1.0
+		5.0, 1.0, 5.0, 1.0
 	]);
 	
 	const planeMesh = new ezgfx.Mesh();
