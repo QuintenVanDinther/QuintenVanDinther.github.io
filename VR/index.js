e6ac39d8a45a395bb467976414a5a066505dd7aa
QuintenVanDinther <55178963+QuintenVanDinther@users.noreply.github.com>
2022-06-21 17:34:45 +0200

    Update index.js

--- VR/index.js
+++ VR/index.js
@@ -271,8 +271,8 @@ function onSessionStarted(_session) { // this function defines what happens when
 				
 				renderer.draw(coneMesh, coneMaterial);
 
-				PlanetRadians += 1;
-				offsetMatrixPlanet = circle(offsetMatrixPlanet, PlanetRadians, 50);
+				PlanetRadians += 0.01;
+				offsetMatrixPlanet = rotate(offsetMatrixPlanet, PlanetRadians, 50);
 				planetMaterial.setModel(offsetMatrixPlanet);
 
 				planetMaterial.setProjection(view.projectionMatrix);
@@ -318,8 +318,8 @@ function onSessionStarted(_session) { // this function defines what happens when
 initWebXR(); // we call our init function, therefore initializing the application
 
 
-function circle(Matrix, Radians, Speed){
-Matrix[12] = Math.cos(Radians ) * (Speed /1000);
-Matrix[14] = Math.sin(Radians) * (Speed /1000);
+function rotate(Matrix, Radians, Speed){
+Matrix[12] = Matrix[12] + Math.cos(Radians) * (Speed /1000);
+Matrix[14] = Matrix[14] + Math.sin(Radians) * (Speed /1000);
 	return Matrix;
 }
