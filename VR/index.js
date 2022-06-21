0e3f4fe4a6bf1d0451c9c22fcb1c38d9fab63cf7
QuintenVanDinther <55178963+QuintenVanDinther@users.noreply.github.com>
2022-06-21 14:51:19 +0200

    Update index.js

--- VR/index.js
+++ VR/index.js
@@ -143,7 +143,7 @@ function onSessionStarted(_session) { // this function defines what happens when
 	planeMaterial.setColor([0.5, 0.5, 0.5, 1.0]);
 
 	const planeTexture = new ezgfx.Texture();
-	planeTexture.loadFromFile("https://media.istockphoto.com/vectors/green-grass-realistic-textured-background-vector-id1208399813?k=20&m=1208399813&s=612x612&w=0&h=UtOKGGTiBu3YT8ZnGleGEytyPHP0039xREo0p32LWaY=", )
+	planeTexture.loadFromFile("./grrass.jpg")
 	planeMaterial.setTexture(planeTexture);
 
 	const cubeMesh = new ezgfx.Mesh();
