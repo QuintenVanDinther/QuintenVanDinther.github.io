d13ee95fb6bb606cebc2b4c1acca433eb9c32bd2
QuintenVanDinther <55178963+QuintenVanDinther@users.noreply.github.com>
2022-06-22 12:29:52 +0200

    Update index.js

--- VR/index.js
+++ VR/index.js
@@ -108,10 +108,10 @@ function onSessionStarted(_session) { // this function defines what happens when
 		0.0, 0.0, 0.0, 1.0
 	]);
 	var offsetMatrix = new Float32Array([
-		1.0, 0.0, 0.0, 0.0,
-		0.0, 0.5, 0.0, 0.0,
-		0.0, 0.0, 1.0, 0.0,
-		-2.0, 1.0, 5.0, 0.5
+		1.0, 0.5, 0.0, 0.0,
+		0.5, 1.0, 0.5, 0.0,
+		0.0, 0.5, 1.0, 0.0,
+		-2.0, 1.0, 5.0, 1.0
 	]);
 	var offsetMatrixWater = new Float32Array([
 		1.0, 0.0, 0.0, 0.0,
